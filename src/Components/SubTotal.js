cf9c4ee1d7eeed57f4486595fe05148e808bd668
AhmedKhaled <ahmedrashad13281@gmail.com>
2023-01-19 23:22:39 +0200

    amazon-clone

--- src/Components/SubTotal.js
+++ src/Components/SubTotal.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import CurrencyFormat from 'react-currency-format'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { getBasketTotal } from '../utils/BasketTotal'
@@ -20,22 +19,15 @@ const SubTotal = () => {
     console.log(getBasketTotal(basket))
   return (
     <div className='bg-[#cccccc]  flex flex-col justify-center items-center m-[auto] md:w-[60%] p-[15px] rounded-lg sm:w-[100%]'>
-        <CurrencyFormat
-            renderText={(value) => (
+        <div>
                 <>
-                    <p className='text-2xl sm:text-xl'>Subtotal ({basket.length} Items) : <strong>{value}</strong></p>
-                    <label htmlFor="check" className='select-none'>
+                    <p className='text-2xl sm:text-xl'>Subtotal ({basket.length} Items) : <strong>{getBasketTotal(basket)}$</strong></p>
+                    <label htmlFor="check" className='select-none ml-[20px]'>
                         <input id="check" type="checkbox"/>
                         This Order Is A Gift
                     </label>
                 </>
-            )}
-            decimalScale={2}
-            value={getBasketTotal(basket)}
-            displayType={"text"}
-            thousandSeparator={true}
-            prefix={"$"}
-        />
+        </div>
         <button className="w-[40%] mt-[15px] bg-[#F7CA00] p-1 flex rounded-lg justify-center items-center" onClick={() => handleCheckout()}>Proceed to Checkout</button>
     </div>
   )
