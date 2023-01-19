09b5518da1cc4ea8e6a8ea0d60b2ee500f703294
AhmedKhaled <ahmedrashad13281@gmail.com>
2023-01-20 00:04:43 +0200

    amazon-clone

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import moment from 'moment/moment'
 import CheckoutProduct from './CheckoutProduct'
-import CurrencyFormat from 'react-currency-format'
 
 
 const Order = ({order}) => {
@@ -16,18 +15,11 @@ const Order = ({order}) => {
                 <CheckoutProduct key={item.id} product={item} hideButton={true}/>
             ))
         }
-        <CurrencyFormat
-            renderText={(value) => (
+        <div>
                 <>
-                    <h3 className='text-center bg-[#cccccc] text-2xl font-bold m-[auto] w-[100%] p-4'>Order Total : {value}</h3>
+                    <h3 className='text-center bg-[#cccccc] text-2xl font-bold m-[auto] w-[100%] p-4'>Order Total : {order.data.amount / 100}</h3>
                 </>
-            )}
-            decimalScale={2}
-            value={order.data.amount / 100}
-            displayType={"text"}
-            thousandSeparator={true}
-            prefix={"$"}
-        />
+        </div>
     </div>
   )
 }
